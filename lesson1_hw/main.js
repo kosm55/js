050db1534e62582623e1e8734ed81d5cd7a496b7
Liudmyla <kosmosjatko@gmail.com>
2023-11-21 13:38:38 +0200

    create index.html and main.js for hw_lesson1

---
+++ lesson1_hw/main.js
@@ -0,0 +1,47 @@
+let st1 = 'hello';
+console.log(st1);
+let st2 = 'owu';
+console.log(st2);
+let st3 = 'com';
+console.log(st3);
+let st4 = 'ua';
+console.log(st4);
+let n1 = 1;
+console.log(n1);
+let n2 = 10;
+console.log(n2);
+let n3 = -999;
+console.log(n3);
+let n4 = 123;
+console.log(n4);
+const PI = 3.14;
+console.log(PI);
+let n5 = 2.7;
+console.log(n5);
+let n6 = 16;
+console.log(n6);
+let b1 = true;
+console.log(b1);
+let b2 = false;
+console.log(b2);
+
+let firstName = 'Yakymenko';
+let middleName = 'YL';
+let lastName = 'Liudmyla';
+// let person = firstName + ' ' + middleName + ' ' + lastName;
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
+
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
+let name1=prompt("Ввести ім'я")
+console.log(name1);
+let name2=prompt("Ввести по-батькові")
+console.log(name2);
+let age = (prompt('Ввести вік'));
+console.log(+age);
