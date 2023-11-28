7f4793c19cee341ceba8549a9d7facae0229d158
Liudmyla <kosmosjatko@gmail.com>
2023-11-28 17:16:04 +0200

    add index3.html and main3.js  hw3 lesson3

---
+++ lesson3_hw/main3.js
@@ -0,0 +1,128 @@
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+let arr1=[{},{},{},{},{},{},{},{},{},{}]
+for (let i=0; i<arr1.length; i++){
+    document.write(`<div>block</div>`)
+}
+
+//----------------------------------------------------
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+let arr2=['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js', 'c#', 'java']
+for (let i=0; i<arr2.length; i++){
+    document.write(`<div>${[i]} skill ${arr2[i]}</div>`)
+}
+//----------------------------------------------------
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+let arr3=[{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}];
+let i2=0;
+while (i2<arr3.length){
+    document.write(`<h1>user info</h1>`)
+    i2++;
+}
+//----------------------------------------------------
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+let arr4=[{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}];
+let i1=0;
+while (i1<arr4.length){
+    document.write(`<h1>user info ${[i1]}</h1>`)
+    i1++;
+}
+
+//----------------------------------------------------
+// - Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+// Масив:
+// ШАБЛОН:
+//     <ul>
+//         <li>ITEM OF ARRAY</li>
+//         <!--
+//             і тд інші об'єкти масиву
+//              ...
+//              ...
+//              ...
+//         -->
+//     </ul>
+//
+// замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
+
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+for (const listOfItem of listOfItems) {
+    document.write(`<ul>
+                        <li>${listOfItem}</li>
+                    </ul>`)}
+
+//----------------------------------------------------
+//     Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+// Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
+
+// ШАБЛОН
+// <div class="product-card">
+//     <h3 class="product-title">TITLE. Price - PRICE</h3>
+// <img src="IMAGE" alt="" class="product-image">
+// </div>
+// Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту
+
+let products = [
+    {
+        title: 'milk',
+        price: 22,
+        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+    },
+    {
+        title: 'juice',
+        price: 27,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+    },
+    {
+        title: 'tomato',
+        price: 47,
+        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+    },
+    {
+        title: 'tea',
+        price: 15,
+        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+    },
+];
+
+for (const product of products) {
+    document.write(`<div class="product-card">
+                        <h3 class="product-title">${product.title}. Price - ${product.price} UAN</h3>
+                        <img src="${product.image}" alt="" class="product-image">
+                    </div>`)
+}
+
+
+//----------------------------------------------------
+//     є масив
+// за допомоги циклу вивести:
+//     - користувачів зі статусом true
+// - користувачів зі статусом false
+// - користувачів які старші за 30 років
+
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
+for (const user of users) {
+    if (user.status===true){
+        console.log((user.status), (user.name))}
+}
+for (const user of users) {
+    if (user.status===false){
+        console.log((user.status), (user.name))}
+}
+for (const user of users) {
+    if (user.age>30){
+        console.log(user)}
+}
