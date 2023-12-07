ef10f806df88446e3406ff2481d1639f54981e5e
Liudmyla <kosmosjatko@gmail.com>
2023-12-07 17:43:56 +0200

    create lesson6_hw index.html and main.js + add additional task other lessons

--- lesson3_hw/add1_main.js
+++ lesson3_hw/add1_main.js
@@ -90,33 +90,142 @@
 
 // 8. вивести масив в зворотньому порядку.
 
-let arr1=[2,3,13,6,22,31,45,66,100,-18];
-for (i=arr1.length; i>0; i--){
-    arr1[i]=arr1[arr1.length];
-}
-console.log(arr1)
+// let arr1=[2,3,13,6,22,31,45,66,100,-18];
+// for (i=arr1.length; i>0; i--){
+//     arr1[i]=arr1[arr1.length];
+// }
+// console.log(arr1)
 
 // 9. всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
 //
 // - Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
+// let arr2=[2,3,13,6,22,31,45,66,100,-18];
+// for (let i = 0; i < arr2.length; i++) {
+//     const arr2Element = arr2[i];
+//     console.log(arr2Element)
+// }
+
 // - Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.
+// let arr=['one', 'two', 'three', 'four', 'fife']
+// for (let i = 0; i < arr.length; i++) {
+//     const arrElement = arr[i];
+//     console.log(arrElement)
+// }
+
 // - Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
-// - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи
-// - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи
-// - Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки рядкові елементи
+// let arr=[true, 'two', 155, 'four', 'fife',-600]
+// for (let i = 0; i < arr.length; i++) {
+//     const arrElement = arr[i];
+//     console.log(arrElement)
+// }
+
+// - Створити масив з 10 елементів числового, стірчкового і булевого типу.
+// За допомогою if та typeof вивести тільки булеві елементи
+// let arr=[true, 'two', 155, 'four', 'fife',-600, false]
+// for (let i = 0; i < arr.length; i++)
+//         if (typeof arr[i] === "boolean" ) {
+//             console.log(arr[i])
+// }
+// - Створити масив з 10 елементів числового, стірчкового і булевого типу.
+// За допомогою if та typeof вивести тільки числові елементи
+// let arr=[true, 'two', 155, 'four', 'fife',-600, false]
+// for (let i = 0; i < arr.length; i++)
+//     if (typeof arr[i]=== "number"){
+//         console.log(arr[i])
+//     }
+
+// - Створити масив з 10 елементів числового, стрічкового і булевого типу.
+// За допомогою if та typeof вивести тільки рядкові елементи
+// let arr=[true, 'two', 155, 'four', 'fife',-600, false]
+// for (let i = 0; i < arr.length; i++) {
+//     if (typeof arr[i]==="string") {
+//         console.log(arr[i])
+//     }
+// }
+
+// - Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів.
+// Вивести в консоль всі його елементи в циклі.
+// let arr=[];
 //
-// - Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів. Вивести в консоль всі його елементи в циклі.
+// arr[0]=1;
+// arr[1]=true;
+// arr[2]='qwersd'
+// arr[3]=899
+//
+// for (let i=0; i<arr.length; i++){
+//
+// }
+// console.log(arr)
+
+
 // - Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+// for (let i = 0; i < 10; i++) {
+//     console.log(`${'step'} ${i+1}`)
+// }
 // - Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+// for (let i = 0; i < 100; i++) {
+//     console.log(`${'step'} ${i+1}`)
+//     }
 // - Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
+// for (let i = 0; i < 100; i+=2) {
+//     console.log(`${'step'} ${i}`)
+//     }
 // - Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
+// for (let i = 2; i < 100; i+=2) {
+//     console.log(`${'step'} ${i+1}`)
+//     }
 // - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
-//
-//
-//
+// for (let i = 0; i < 100; i+=2) {
+//     console.log(`${'step'} ${i+1}`)
+// }
 // стоврити масив книжок (назва, кількість сторінок, автори , жанри).
-// -знайти наібльшу книжку.
-// - знайти книжку/ки з найбільшою кількістю жанрів
-// - знайти книжку/ки з найдовшою назвою
-// - знайти книжку/ки які писали 2 автори
-// - знайти книжку/ки які писав 1 автор
+// let books=[
+//     { title: 'tit1', page: 200, author: ['jason'], genre: ['science']},
+//     {title: 'tit2222', page: 500, author: ['moreti'], genre: ['poem', 'drama']},
+//     {title: 'tit33', page: 25, author: ['gote','devon'], genre: ['comedy']},
+//     {title: 'tit444444', page: 851, author: ['robert', 'erih'], genre: ['drama']}
+// ]
+// // -знайти наібльшу книжку.
+// let bigestPage=books[0].page;
+// for (let i = 0; i < books.length; i++) {
+//         if (books[i].page>bigestPage){
+//         bigestPage=books[i].page
+//     }
+// }
+// console.log(bigestPage)
+//
+// // - знайти книжку/ки з найбільшою кількістю жанрів
+// let bigestGanre =books[0].genre.length
+// for (let i=0; i<books.length; i++){
+//     if (books[i].genre.length>bigestGanre){
+//         bigestGanre=books[i].genre
+//     }
+// }
+// console.log(bigestGanre)
+//
+// // - знайти книжку/ки з найдовшою назвою
+// let bigestTitle=books[0].title;
+// for (let book of books) {
+//     if (book.title.length>bigestTitle.length){
+//         bigestTitle=book.title
+//     }
+// }
+// console.log(bigestTitle)
+// // - знайти книжку/ки які писали 2 автори
+//
+// let moreBooksAuthors=[];
+// for (const book of books) {
+//     if (book.author.length === 2){
+//         moreBooksAuthors[moreBooksAuthors.length]=book
+//     }
+// }
+// console.log(moreBooksAuthors)
+//
+// // - знайти книжку/ки які писав 1 автор
+// let moreBooksOneAuthor=[];
+// for (const book of books) {
+//     if (book.author.length === 1){
+//         moreBooksOneAuthor[moreBooksOneAuthor.length]=book
+//     }
+// }
+// console.log(moreBooksOneAuthor)
---
+++ lesson6_hw/main.js
@@ -0,0 +1,280 @@
+// + Знайти та вивести довижину наступних стрінгових значень
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+let a ='hello world'
+let b='lorem ipsum';
+let c= 'javascript is cool'
+console.log(a.length, b.length, c.length)
+
+
+// let str = ['hello world', 'lorem ipsum', 'javascript is cool'];
+// for (let i = 0; i < str.length; i++) {
+//     console.log(str[i].length); вивидить просто числа , зробити пустий масив куди буде записувати довжину і вивсети цей масив з довжинами, або щось з інедксами зробить щоб виводило "стрінг + його дожину"
+// }
+
+// + Перевести до великого регістру наступні стрінгові значення
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+
+let a1 ='hello world'
+let b1='lorem ipsum';
+let c1= 'javascript is cool'
+console.log(a.toUpperCase(),b.toUpperCase(),c.toUpperCase())
+
+
+// + Перевести до нижнього регістру настипні стрінгові значення
+// 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+
+let a2 ='HELLO WORLD'
+let b2='LOREM IPSUM';
+let c2= 'JAVASCRIPT IS COOL'
+console.log(a.toLowerCase(),b.toLowerCase(),c.toLowerCase())
+
+
+// + Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+
+let str = ' dirty string   '
+console.log(str.trim())
+
+// let str = ' dirty string   '
+// console.log(str.replaceAll(' ',''))
+
+// + Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+//     let str = 'Ревуть воли як ясла повні';
+// let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
+let str1 = 'Ревуть воли як ясла повні';
+let stringToarray = str1.split(' ')
+console.log(stringToarray)
+
+// + є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
+
+let arrNums=[10,8,-7,55,987,-1011,0,1050,0]
+   let newArrNums = arrNums.map((value)=>{
+        return (value +'')
+})
+console.log(newArrNums)
+
+// + створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого,
+// або навпаки в залежності від значення аргументу direction.
+//     let nums = [11,21,3];
+// sortNums(nums,'ascending') // [3,11,21]
+// sortNums(nums,'descending') // [21,11,3]
+
+let nums = [11,21,3];
+function sortNums(nums,direction){
+    return nums.sort((num1, num2)=>{
+        if (  direction === 'ascending') {
+            return num1-num2
+        }else if
+        (  direction === 'descending'){
+        return num2-num1
+    }
+    })
+}
+console.log(sortNums(nums, 'descending'))
+
+// ==========================
+// + є масив
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+// + відсортувати його за спаданням за monthDuration
+
+let sortCoursesAndDurationArray=coursesAndDurationArray.sort((cours1, cours2)=>{
+    return cours1.monthDuration- cours2.monthDuration
+})
+console.log(sortCoursesAndDurationArray)
+
+// + відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+    let filterCoursesAndDurationArray = coursesAndDurationArray.filter((item) => {
+    if (item.monthDuration>5 ){
+        return true
+    }
+})
+console.log(filterCoursesAndDurationArray)
+
+// + за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+let mapCoursesAndDurationArray= coursesAndDurationArray.map((value, index) => {
+    let newCours= {
+        title: value.title,
+        monthDuration: value.monthDuration,
+        id: index + 1
+    }
+    //    let newCours={...value, id:index+1}
+    return newCours
+})
+console.log(mapCoursesAndDurationArray)
+// =========================
+//   +  описати колоду карт (від 6 до туза без джокерів)
+//
+// {
+//     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
+//         value: '', // '6'-'10', 'ace','jack','queen','king'
+//     color:'', // 'red','black'
+// }
+//
+let values=['6','7','8','9','10','jack','queen','king','ace']
+let cardSuits =['spade', 'diamond','heart', 'clubs']
+
+let cards=[];
+
+for (const value of values) {
+    for (const cardSuit of cardSuits) {
+        let color;
+        if (cardSuit==='spade' || cardSuit==='clubs'){
+            color ='black'
+        }else{
+            color ='red'
+        }
+        let card={cardSuit:cardSuit, value:value, color:color}
+        cards.push(card)
+    }
+}
+console.log(cards)
+
+//----------------------
+// + знайти піковий туз
+
+let findAceSpade=cards.filter(item=>{
+    return (item.value==='ace' && item.cardSuit=== 'spade')
+})
+console.log(findAceSpade)
+//----------------------------
+// + всі шістки
+
+let findSix=cards.filter(item=>{
+    return item.value==='6'
+})
+console.log(findSix)
+
+//---------------------------
+// + всі червоні карти
+
+let redCards= cards.filter(item=>{
+    return item.color==='red'
+})
+console.log(redCards)
+//---------------------------
+// + всі буби
+let findDiamond= cards.filter(item=>{
+    return item.cardSuit ==='diamond'
+})
+console.log(findDiamond)
+
+
+// + всі трефи від 9 та більше
+
+let findClubsAfterNine = cards.filter(item=>{
+    return  ((item.cardSuit==='clubs') && (item.value!=='6' && item.value!=='7' && item.value!=='8'))
+})
+console.log(findClubsAfterNine)
+// =========================
+//
+//   + Взяти описану колоду карт, та за допомоги reduce упакувати всі карти по "мастях" в об'єкт
+// {
+//     spades:[],
+//         diamonds:[],
+//     hearts:[],
+//     clubs:[]
+// }
+// 'spade', 'diamond','heart', 'clubs'
+let reduceCards = cards.reduce((acumulator, card)=>{
+    if (card.cardSuit==='spade'){
+        acumulator.spades.push(card)
+    }else if (card.cardSuit==='diamond'){
+        acumulator.diamonds.push(card)
+    }else if (card.cardSuit==='heart'){
+        acumulator.hearts.push(card)
+    }else {
+        acumulator.clubs.push(card)
+    }
+    return acumulator
+    },{
+    spades:[],
+    diamonds:[],
+    hearts:[],
+    clubs:[]
+})
+console.log(reduceCards)
+// =========================
+//     взяти з arrays.js (який лежить в папці 2023 plan) масив coursesArray
+
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'git', 'java core', 'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'python core', 'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html', 'css','js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js', 'python', 'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+
+
+// + написати пошук всіх об'єктів, в який в modules є sass
+//виводить об'єкт
+for (let coursesArrayElement of coursesArray) {
+let filterModulesOfCoursesArray=coursesArrayElement.modules.filter((item)=>{
+    for (let item of coursesArrayElement.modules) {
+        if (item==='sass'){
+            return true
+        }
+    }
+})
+    if (filterModulesOfCoursesArray.length>0){
+    console.log(coursesArrayElement)
+    }
+}
+
+//виводить масив об'єктів
+// let filterModulesOfCoursesArray=coursesArray.filter((item)=>item.modules.includes('sass'))
+//     console.log(filterModulesOfCoursesArray)
+
+
+
+//перебір циклом
+// for (const coursesArrayElement of coursesArray) {
+//     let resCours= coursesArrayElement.modules
+//     for (let item of resCours) {
+//         if (item==='sass'){
+//         console.log(coursesArrayElement)
+//         }
+//     }
+// }
+
+
+// --написати пошук всіх об'єктів, в який в modules є docker
+
+let filtrDocer= coursesArray.filter(modul=>modul.modules.includes('docker'))
+console.log(filtrDocer)
